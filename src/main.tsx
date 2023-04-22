06e31093ac62e76206f8546c57e9ff770edb229e
pho0m <pho0m2000s@gmail.com>
2023-04-22 10:28:27 +0700

    fix(css): import css for use

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./main.css";
-import "react-bubble-ui/dist/index.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.Suspense>
