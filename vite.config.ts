6289d08650c3250ce98b527fd767920421a9a9bb
pho0m <pho0m2000s@gmail.com>
2023-04-22 09:28:41 +0700

    fix(config): for build

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,14 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  define: {
+    global: {},
+  },
+  resolve: {
+    alias: {
+      "./runtimeConfig": "./runtimeConfig.browser",
+    },
+  },
   server: {
     port: 8081,
   },
